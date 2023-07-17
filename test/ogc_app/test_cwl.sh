38ea88075a7bbc745cae8e370249eab5fbff0697
James McDuffie <James.McDuffie@jpl.nasa.gov>
2023-07-17 15:01:01 -0700

    Update test inputs for changes to app-pack-generator to use unity-data-services:5.2.1. Also add modification of job input file with AWS credentials from the environment variables.

--- test/ogc_app/test_cwl.sh
+++ test/ogc_app/test_cwl.sh
@@ -16,6 +16,37 @@ if [ ! -e $JOB_INP_FILENAME ]; then
     exit 1
 fi
 
+# Write environment variables for AWS credentials into job input file if not already null
+declare -A job_input_overrides
+job_input_overrides['aws_access_key_id']=$AWS_ACCESS_KEY_ID
+job_input_overrides['aws_secret_access_key']=$AWS_SECRET_ACCESS_KEY
+job_input_overrides['aws_session_token']=$AWS_SESSION_TOKEN
+job_input_overrides['aws_region']=$AWS_DEFAULT_REGION
+job_input_overrides['edl_username']=$EDL_USERNAME
+job_input_overrides['edl_password']=$EDL_PASSWORD
+
+# Create temporary copy of job input file 1
+modified_job_inp_file=$(mktemp --suffix=".cwl_job.cwl")
+cp -v "$JOB_INP_FILENAME" "$modified_job_inp_file"
+
+# Modify the cmr_results.json path to be absolute to the script path
+sed -i 's|cmr_results.json|'"$SCRIPT_DIR"'/cmr_results.json|' $modified_job_inp_file
+
+# Override values that have environment variables defined for them
+for job_input_var_name in "${!job_input_overrides[@]}"; do
+    job_inp_var_value=${job_input_overrides[$job_input_var_name]}
+
+    if [ ! -z "$job_inp_var_value" ]; then
+        sed -i 's|'"${job_input_var_name}"': .*$|'"${job_input_var_name}"': "'"${job_inp_var_value}"'"|' "$modified_job_inp_file"
+    fi
+done
+
+echo "Using modified job input file $modified_job_inp_file:"
+cat $modified_job_inp_file
+
 cwltool \
     --debug --leave-tmpdir --no-read-only \
-    $WORKFLOW_FILENAME $JOB_INP_FILENAME
+    "$WORKFLOW_FILENAME" "$modified_job_inp_file" \
+    $*
+
+rm $modified_job_inp_file
