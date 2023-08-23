9915851646543efb554c3e3d556aad7f7269c6fa
James McDuffie <James.McDuffie@jpl.nasa.gov>
2023-08-23 09:58:22 -0700

    Update test script to use new executable name for unity_app_gen

--- test/ogc_app/build_app.sh
+++ test/ogc_app/build_app.sh
@@ -6,7 +6,7 @@ BASE_DIR=$(realpath $(dirname $0)/../..)
 
 cd $BASE_DIR
 
-build_ogc_app.py init .
+build_ogc_app init .
 
-build_ogc_app.py build_docker
-build_ogc_app.py build_cwl
+build_ogc_app build_docker
+build_ogc_app build_cwl
